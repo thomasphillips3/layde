648070abffcfd9d4577c852b92c543384bff59d4
Thomas Phillips <thomasphillips3@gmail.com>
2024-12-20 13:39:19 -0800

    define custom colors

---
+++ Source/LookAndFeel.h
@@ -0,0 +1,22 @@
+#include <JuceHeader.h>
+
+namespace Colors
+{
+    const juce::Colour background { 245, 240, 235 };
+    const juce::Colour header { 40, 40, 40 };
+
+    namespace Knob
+    {
+        const juce::Colour trackBackground { 205, 200, 195 };
+        const juce::Colour trackActive { 177, 101, 135 };
+        const juce::Colour outline { 255, 250, 245 };
+        const juce::Colour gradientTop { 250, 245, 240 };
+        const juce::Colour gradientBottom { 240, 235, 230 };
+        const juce::Colour dial { 100, 100, 100 };
+        const juce::Colour dropShadow { 195, 190, 185 };
+        const juce::Colour label { 80, 80, 80 };
+        const juce::Colour textBoxBackground { 80, 80, 80 };
+        const juce::Colour value { 240, 240, 240 };
+        const juce::Colour caret { 255, 255, 255 };
+    }
+}
